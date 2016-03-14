2575496423f7b2cd1b60f8811c372ff2a5e134f7
Lachlan Gartside <lachlang@gmail.com>
2016-03-14 16:48:15 +1100

    played with layout and controller implementation

--- public/components/calculators/calculatorsController.js
+++ public/components/calculators/calculatorsController.js
@@ -15,4 +15,9 @@ brewTrackerApp.controller('CalculatorsCtrl', ['$scope', '$log', function($scope,
 	$scope.gravity = function(){
 		return $scope.BMA * $scope.PPG / $scope.batch;
 	}
+
+	this.bma = 10;
+	this.grist = 3;
+	this.batch = 6;
+	this.gravityValue = this.bma * this.grist / this.batch;
 }]);
